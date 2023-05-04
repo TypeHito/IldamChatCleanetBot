616c46c5cd9a804573dfcd8f4653f06aae340abe
TypeHito <NoneType.Dev@gmail.com>
2023-05-05 02:13:06 +0500

    replece ' to " and edit comments

--- main.py
+++ main.py
@@ -4,59 +4,63 @@ import requests
 from flask_sslify import SSLify
 
 app = Flask(__name__)
+# set ssl
 sslify = SSLify(app)
 
-"""Global"""
-TOKEN = '''6274568989:AAEwhaXFHstZ3hP7KFAfJCgrWJ9VpqTLG4s'''
-hook_url = "IldamTeam.pythonanywhere.com"
+# """Global"""
+# TOKEN = """6274568989:AAEwhaXFHstZ3hP7KFAfJCgrWJ9VpqTLG4s"""
+# hook_url = "IldamTeam.pythonanywhere.com"
 
 """Local"""
-# TOKEN = '''5178937763:AAEu_BV7xGPvD0WOGzjm0iZJE4m3z0NRzVc'''
-# hook_url = "https://e302-213-230-82-243.eu.ngrok.io"
+TOKEN = """6274568989:AAEwhaXFHstZ3hP7KFAfJCgrWJ9VpqTLG4s"""
+hook_url = "https://e302-213-230-82-243.eu.ngrok.io"
 
-
-URL = '''https://api.telegram.org/bot{}/'''.format(TOKEN)
+# TypeHito TId = 5296922626
+admin = 5296922626
+URL = """https://api.telegram.org/bot{}/""".format(TOKEN)
+hook_status = False
 
 rules = ["new_chat_member", "new_chat_members", "left_chat_member", "new_chat_photo", "new_chat_title",
          "delete_chat_photo", "supergroup_chat_created"]
 
 
 def resethook():
-    del_web_hook = f'https://api.telegram.org/bot{TOKEN}/deleteWebhook'
-    set_web_hook = f'https://api.telegram.org/bot{TOKEN}/setWebhook?url={hook_url}'
-    print(requests.post(del_web_hook))
-    print(requests.post(set_web_hook))
+    del_web_hook = f"https://api.telegram.org/bot{TOKEN}/deleteWebhook"
+    set_web_hook = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={hook_url}"
+    del_hook_status = requests.post(del_web_hook)
+    set_hook_status = requests.post(set_web_hook)
+    send_message(admin, f"Hook del status: {del_hook_status}\nHook set status: {set_hook_status}")
 
 
-def send_message(chat_id, text, parse_mode='markdown', reply_markup=None):
-    url = URL + 'sendMessage'
+def send_message(chat_id, text, parse_mode="markdown", reply_markup=None):
+    url = URL + "sendMessage"
     if reply_markup:
-        anw = {'chat_id': chat_id, 'text': text, 'parse_mode': parse_mode, 'reply_markup': reply_markup}
+        req = {"chat_id": chat_id, "text": text, "parse_mode": parse_mode, "reply_markup": reply_markup}
     else:
-        anw = {'chat_id': chat_id, 'text': text, 'parse_mode': parse_mode}
-    r = requests.post(url, json=anw)
-    return r.json()
-
-
-def delete_message(chat_id, message_id):
-    url = URL + 'deleteMessage'
-    req = {'chat_id': chat_id, "message_id": message_id}
+        req = {"chat_id": chat_id, "text": text, "parse_mode": parse_mode}
     r = requests.post(url, json=req)
     return r.json()
 
 
-@app.route("/", methods=['POST', 'GET'])
+def delete_message(chat_id, message_id):
+    url = URL + "deleteMessage"
+    req = {"chat_id": chat_id, "message_id": message_id}
+    r = requests.post(url, json=req)
+    return r.json()
+
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    if f_request.method == 'POST':
+    if f_request.method == "POST":
 
         r = f_request.get_json()
-        message = r.get('message')
-        print(r)
+        message = r.get("message")
+
         if message:
             try:
-                chat_id = message['chat']['id']
+                chat_id = message["chat"]["id"]
             except Exception as err:
-                print("Chat or ChatId not Found!: \n", err)
+                send_message(admin, f"Chat or ChatId not Found!: \n{err}\n{r}")
                 chat_id = None
 
             for rule in rules:
@@ -65,18 +69,20 @@ def index():
                     delete_message(chat_id, message["message_id"])
 
             if message.get("new_chat_member"):
-                send_message(chat_id, f"Welcome! {message.get(message['from']['username'])}")
+                send_message(chat_id, f"""Welcome! {message.get(message["from"]["username"])}""")
                 return {"ok": True}
 
             if message.get("text") == "test":
-                send_message(5296922626, "Test Finished")
+                print(send_message(admin, "Test Finished"))
                 return {"ok": True}
         return {"ok": True}
-    return "<a href='https://t.me/typeHito'><h1>Owner</h1>"
+    return "<a href=\"https://t.me/typeHito\"><h1>Owner</h1>"
 
 
-if __name__ == '__main__':
+if not hook_status:
     resethook()
+    hook_status = True
+
+if __name__ == "__main__":
     send_message(5296922626, "Bot HasBeen started")
     app.run()
-
