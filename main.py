00ed41052983f2e154b6c2ceb45133311c91d2b3
TypeHito <NoneType.Dev@gmail.com>
2023-05-05 01:40:55 +0500

    fixed add from invite link

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ from flask_sslify import SSLify
 app = Flask(__name__)
 sslify = SSLify(app)
 
+"""Global"""
 TOKEN = '''6274568989:AAEwhaXFHstZ3hP7KFAfJCgrWJ9VpqTLG4s'''
 hook_url = "IldamTeam.pythonanywhere.com"
+
+"""Local"""
+# TOKEN = '''5178937763:AAEu_BV7xGPvD0WOGzjm0iZJE4m3z0NRzVc'''
+# hook_url = "https://e302-213-230-82-243.eu.ngrok.io"
+
+
 URL = '''https://api.telegram.org/bot{}/'''.format(TOKEN)
+
+rules = ["new_chat_member", "new_chat_members", "left_chat_member", "new_chat_photo", "new_chat_title",
+         "delete_chat_photo", "supergroup_chat_created"]
 
 
 def resethook():
@@ -35,38 +45,38 @@ def delete_message(chat_id, message_id):
     return r.json()
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods=['POST', 'GET'])
 def index():
     if f_request.method == 'POST':
 
         r = f_request.get_json()
         message = r.get('message')
-
+        print(r)
         if message:
-            new_chat_member = message.get("new_chat_member")
-            new_chat_members = message.get("new_chat_members")
-            left_chat_member = message.get("left_chat_member")
-            new_chat_photo = message.get("new_chat_photo")
-
             try:
                 chat_id = message['chat']['id']
             except Exception as err:
                 print("Chat or ChatId not Found!: \n", err)
                 chat_id = None
-            if message["text"] == "test":
-                send_message(5296922626, "Welcome! ")
-            if new_chat_member or new_chat_members or left_chat_member or new_chat_photo:
-                # Here deleting message from chat
-                delete_message(chat_id, message["message_id"])
 
-            if new_chat_member:
-                send_message(chat_id, "Welcome! ")
+            for rule in rules:
+                if message.get(rule):
+                    # Here deleting message from chat
+                    delete_message(chat_id, message["message_id"])
 
+            if message.get("new_chat_member"):
+                send_message(chat_id, f"Welcome! {message.get(message['from']['username'])}")
+                return {"ok": True}
+
+            if message.get("text") == "test":
+                send_message(5296922626, "Test Finished")
+                return {"ok": True}
         return {"ok": True}
-    return "<a href='https://t.me/typeHito'><h1>join to channel</h1>"
+    return "<a href='https://t.me/typeHito'><h1>Owner</h1>"
 
 
 if __name__ == '__main__':
-    send_message(5296922626, "Bot HassBeen started")
+    resethook()
+    send_message(5296922626, "Bot HasBeen started")
     app.run()
 
