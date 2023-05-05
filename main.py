c177d54e9c14636fb007bba6c453a49379cc69cb
TypeHito <NoneType.Dev@gmail.com>
2023-05-05 17:51:44 +0500

    Pin message delete update And Welcome Message Deleted Append Token To Url

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 # set ssl
 sslify = SSLify(app)
 
-# """Global"""
-# TOKEN = """6274568989:AAEwhaXFHstZ3hP7KFAfJCgrWJ9VpqTLG4s"""
-# hook_url = "IldamTeam.pythonanywhere.com"
-
-"""Local"""
+"""Global"""
 TOKEN = """6274568989:AAEwhaXFHstZ3hP7KFAfJCgrWJ9VpqTLG4s"""
-hook_url = "https://e302-213-230-82-243.eu.ngrok.io"
+hook_url = "IldamTeam.pythonanywhere.com"
+#
+# """Local"""
+# TOKEN = """5178937763:AAGB0hP4rMqzRfALYJUrileEOAY5zonIETk"""
+# hook_url = "https://c850-213-230-82-243.eu.ngrok.io"
 
 # TypeHito TId = 5296922626
 admin = 5296922626
@@ -21,12 +21,12 @@ URL = """https://api.telegram.org/bot{}/""".format(TOKEN)
 hook_status = False
 
 rules = ["new_chat_member", "new_chat_members", "left_chat_member", "new_chat_photo", "new_chat_title",
-         "delete_chat_photo", "supergroup_chat_created"]
+         "delete_chat_photo", "supergroup_chat_created", "pinned_message"]
 
 
 def reset_hook():
     del_web_hook = f"https://api.telegram.org/bot{TOKEN}/deleteWebhook"
-    set_web_hook = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={hook_url}"
+    set_web_hook = f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={hook_url}{TOKEN}"
     del_hook_status = requests.post(del_web_hook)
     set_hook_status = requests.post(set_web_hook)
     send_message(admin, f"Hook del status: {del_hook_status}\nHook set status: {set_hook_status}")
@@ -49,7 +49,7 @@ def delete_message(chat_id, message_id):
     return r.json()
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/"+TOKEN, methods=["POST", "GET"])
 def index():
     if f_request.method == "POST":
 
@@ -60,17 +60,13 @@ def index():
             try:
                 chat_id = message["chat"]["id"]
             except Exception as err:
-                send_message(admin, f"Chat or ChatId not Found!: \n{err}\n{r}")
+                send_message(admin, f"Chat or Chat_ID not Found!: \n{err}\n{r}")
                 chat_id = None
 
             for rule in rules:
                 if message.get(rule):
                     # Here deleting message from chat
                     delete_message(chat_id, message["message_id"])
-
-            if message.get("new_chat_member"):
-                send_message(chat_id, f"""Welcome! {message.get(message["from"]["username"])}""")
-                return {"ok": True}
 
             if message.get("text") == "test":
                 print(send_message(admin, "Test Finished"))
@@ -79,7 +75,7 @@ def index():
     return "<a href=\"https://t.me/typeHito\"><h1>Owner</h1>"
 
 
-if not hook_status:
+if hook_status is False:
     reset_hook()
     hook_status = True
 
